f393f171df3d122ac5ade1a61a27946eb83a31cb
SystemTN <trungnq92@gmail.com>
2017-10-20 06:14:55 +0700

    Update default.py

--- default.py
+++ default.py
@@ -23,6 +23,7 @@ def add_video_item(url, infolabels, img=''):
 
 add_video_item('https://od.lk/d/ODVfMzMyODAzM18/Save.Me.Ep03.part1.fix.mp4',{ 'title': 'JadViE'}, icon)
 add_video_item('https://fsb1.weshare.me/f007911de9ad2762/BEN_V_MUN_1H.mp4?download_token=c81ad062ee814d98b4ad3c3c8a4f255f36828c17037a18a22b5a456d77d93e8c',{ 'title': 'Football'}, icon)
+add_video_item('https://filedn.com/lz8bImMGXfW0qwGWxcotF35/Because.This.Is.My.First.Life.E01.171009.360p-NEXT.mkv',{ 'title': 'Football'}, icon)
 xbmcplugin.endOfDirectory(plugin_handle)
 sys.exit(0)
 
