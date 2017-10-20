9782f97623b55db3ef3c2c5f6e5f9dd479aa74bf
SystemTN <trungnq92@gmail.com>
2017-10-20 07:31:36 +0700

    Update default.py

--- default.py
+++ default.py
@@ -22,7 +22,8 @@ def add_video_item(url, infolabels, img=''):
 
 add_video_item('http://download013.fshare.vn/dl/0YagMgGelKfM5DLYYJG2TsEV31WqYBpbYnMwtWXLO56FAp1Z0K5084MSg9-ndoyJxzLXJ7PtS9BzEgrH/Minions.2015.ViE.720p.HC.HDRip.X264.AC3-Toy.mkv',{ 'title': 'FShare.VN'}, icon)
 add_video_item('https://od.lk/d/ODVfMzMyODAzM18/Save.Me.Ep03.part1.fix.mp4',{ 'title': 'OpenDrive'}, icon)
-add_video_item('https://och8og.bn1303.livefilestore.com/y4m5rkOZBvOOJ-gGpplG-jqWtc-nSidUbjuJG98DrP6sDmHVStCpsXrsC1zExxfhDWPjOHVV2KWFqFYvXDv83P1ogMoo_LPetbzgZ9Y_HPl-erNXXkcYtf84ukeAxB5Qt29teJa3OkrBrdXGvSw1rYxiXVpR9kySuJHyhArZ42TJmm5ussNWfZ-8O-ZrV9VUH0Q/Buddies.in.India.2017.TM.mp4?download&psid=1',{ 'title': 'OneDrive'}, icon)
+add_video_item('https://och8og.bn1303.livefilestore.com/y4m5rkOZBvOOJ-gGpplG-jqWtc-nSidUbjuJG98DrP6sDmHVStCpsXrsC1zExxfhDWPjOHVV2KWFqFYvXDv83P1ogMoo_LPetbzgZ9Y_HPl-erNXXkcYtf84ukeAxB5Qt29teJa3OkrBrdXGvSw1rYxiXVpR9kySuJHyhArZ42TJmm5ussNWfZ-8O-ZrV9VUH0Q/Buddies.in.India.2017.TM.mp4?download&psid=1',{ 'title': 'Buddies in India 2017 - Thuyết Minh - Đại Náo Thiên Trúc 2017'}, https://i.moveek.com/media/cache/poster/media/cef8eb6517ee00a078347f51da0bcd27453ba27d.jpg)
+add_video_item('https://onedrive.live.com/download?cid=295BD6ACA1A92117&resid=295BD6ACA1A92117%219966&authkey=AKeM3zwrHZCvgQ4',{ 'title': 'Detective Dee 2017 - Thuyết Minh - Địch Nhân Kiệt Kỳ Án 2017'}, https://vuviphim.com/wp-content/uploads/2017/05/Dich-Nhan-Kiet-Ky-An-Detective-Dee-2017-Poster.jpg)
 add_video_item('https://fsb1.weshare.me/f007911de9ad2762/BEN_V_MUN_1H.mp4?download_token=c81ad062ee814d98b4ad3c3c8a4f255f36828c17037a18a22b5a456d77d93e8c',{ 'title': 'Football'}, icon)
 add_video_item('https://filedn.com/lz8bImMGXfW0qwGWxcotF35/Because.This.Is.My.First.Life.E01.171009.360p-NEXT.mkv',{ 'title': 'pCloud'}, icon)
 xbmcplugin.endOfDirectory(plugin_handle)
